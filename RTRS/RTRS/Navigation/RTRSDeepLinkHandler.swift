c83adffb5fe709232583d8df735fdf9200931115
Jonathan Chen <jonchen42@gmail.com>
2019-11-06 11:38:37 -0500

    Some deep link stuff

--- RTRS/RTRS/Navigation/RTRSDeepLinkHandler.swift
+++ RTRS/RTRS/Navigation/RTRSDeepLinkHandler.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftSoup
 
 struct RTRSDeepLinkPayload {
     let baseURL: URL
@@ -22,35 +23,53 @@ class RTRSDeepLinkHandler: NSObject {
     
     static func route(payload: RTRSDeepLinkPayload, navController: RTRSNavigationController) {
         let url = payload.baseURL
-        var vc: UIViewController?
-        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
         
-        if url.path.contains("podcast") {
-            let vm = RTRSNavigation.shared.viewModel(for: .podcasts)
+            var vc: UIViewController?
+            let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
             
-            
-        } else if url.path.contains("if-not-will-convey-as-two-second-rounders") {
-            if let vm = RTRSNavigation.shared.viewModel(for: .au) as? AUCornerMultiArticleViewModel {
-                let articles = vm.content.filter({ (vm) -> Bool in
-                    guard let articleVm = vm as? AUCornerSingleArticleViewModel, let articleUrl = articleVm.baseURL else { return false }
-                    return articleUrl == url
-                })
+            if url.path.contains("podcast") || url.absoluteString.contains("bit.ly") {
+                do {
+                    let 
+                    let document = Document(url.absoluteString)
+                    let h1Elems = try document.getElementsByTag("h1")
+                    let titleElems = h1Elems.filter { (element) -> Bool in
+                        element.hasClass("title")
+                    }
+                    if let titleElem = titleElems.first {
+                        let text = try titleElem.text()
+                        let vm =
+                        
+                        
+                        
+                    }
+                    
                 
-                if articles.count > 0, let article = articles.first as? AUCornerSingleArticleViewModel {
-                    
-                    let vc = storyboard.instantiateViewController(withIdentifier: "AUSingleArticle") as! AUCornerArticleViewController
-                    vc.viewModel = article
-                    
-                    navController.present(AUCornerArticleViewController(), animated: true
-                        , completion: nil)
+                } catch {
+                    return
                 }
+            } else if url.path.contains("if-not-will-convey-as-two-second-rounders") {
+                if let vm = RTRSNavigation.shared.viewModel(for: .au) as? AUCornerMultiArticleViewModel {
+                    let articles = vm.content.filter({ (vm) -> Bool in
+                        guard let articleVm = vm as? AUCornerSingleArticleViewModel, let articleUrl = articleVm.baseURL else { return false }
+                        return articleUrl == url
+                    })
+                    
+                    if articles.count > 0, let article = articles.first as? AUCornerSingleArticleViewModel {
+                        
+                        let vc = storyboard.instantiateViewController(withIdentifier: "AUSingleArticle") as! AUCornerArticleViewController
+                        vc.viewModel = article
+                        
+                        navController.present(AUCornerArticleViewController(), animated: true
+                            , completion: nil)
+                    }
+                }
+            } else {
+                let vc = storyboard.instantiateViewController(withIdentifier: "RTRSExternalWebViewController") as! RTRSExternalWebViewController
+                vc.name = payload.title
+                vc.url = payload.baseURL
+                navController.present(vc, animated: true, completion: nil)
             }
-        } else {
-            let vc = storyboard.instantiateViewController(withIdentifier: "RTRSExternalWebViewController") as! RTRSExternalWebViewController
-            vc.name = payload.title
-            vc.url = payload.baseURL
-            navController.present(vc, animated: true, completion: nil)
-        }
+        
     }
     
     fileprivate static func routeArticle(payload: RTRSDeepLinkPayload) {
